9f315418c1b1d56d59cbb6f4f4af3462a8cc57d1
Shaik Mohammad <153307431+Rehaan3399@users.noreply.github.com>
2026-06-15 12:08:25 +0530

    Create main.py

---
+++ Day-9-OOP-Encapsulation/main.py
@@ -0,0 +1,260 @@
+class Person:
+
+  def __init__(self,name_input,country_input):
+    self.name = name_input
+    self.country = country_input
+
+  def greet(self):
+    if self.country == 'india':
+      print('Namaste',self.name)
+    else:
+      print('Hello',self.name)
+
+
+
+# object without a reference
+class Person:
+
+  def __init__(self):
+    self.name = 'nitish'
+    self.gender = 'male'
+
+p = Person()
+q = p
+
+
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+# outside the class -> function
+def greet(person):
+  print('Hi my name is',person.name,'and I am a',person.gender)
+  p1 = Person('ankit','male')
+  return p1
+
+p = Person('nitish','male')
+x = greet(p)
+print(x.name)
+print(x.gender)
+
+
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+# outside the class -> function
+def greet(person):
+  print(id(person))
+  person.name = 'ankit'
+  print(person.name)
+
+p = Person('nitish','male')
+print(id(p))
+greet(p)
+print(p.name)
+
+
+#Object Mutability
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+# outside the class -> function
+def greet(person):
+  person.name = 'ankit'
+  return person
+p = Person('nitish','male')
+print(id(p))
+p1 = greet(p)
+print(id(p1))
+
+
+
+# instance var -> python tutor
+class Person:
+
+  def __init__(self,name_input,country_input):
+    self.name = name_input
+    self.country = country_input
+
+p1 = Person('nitish','india')
+p2 = Person('steve','australia')
+
+
+
+
+class Atm:
+
+  # constructor(special function)->superpower -> 
+  def __init__(self):
+    print(id(self))
+    self.pin = ''
+    self.__balance = 0
+    #self.menu()
+
+  def get_balance(self):
+    return self.__balance
+
+  def set_balance(self,new_value):
+    if type(new_value) == int:
+      self.__balance = new_value
+    else:
+      print('beta bahot maarenge')
+
+  def __menu(self):
+    user_input = input("""
+    Hi how can I help you?
+    1. Press 1 to create pin
+    2. Press 2 to change pin
+    3. Press 3 to check balance
+    4. Press 4 to withdraw
+    5. Anything else to exit
+    """)
+
+    if user_input == '1':
+      self.create_pin()
+    elif user_input == '2':
+      self.change_pin()
+    elif user_input == '3':
+      self.check_balance()
+    elif user_input == '4':
+      self.withdraw()
+    else:
+      exit()
+
+  def create_pin(self):
+    user_pin = input('enter your pin')
+    self.pin = user_pin
+
+    user_balance = int(input('enter balance'))
+    self.__balance = user_balance
+
+    print('pin created successfully')
+
+  def change_pin(self):
+    old_pin = input('enter old pin')
+
+    if old_pin == self.pin:
+      # let him change the pin
+      new_pin = input('enter new pin')
+      self.pin = new_pin
+      print('pin change successful')
+    else:
+      print('nai karne de sakta re baba')
+
+  def check_balance(self):
+    user_pin = input('enter your pin')
+    if user_pin == self.pin:
+      print('your balance is ',self.__balance)
+    else:
+      print('chal nikal yahan se')
+
+  def withdraw(self):
+    user_pin = input('enter the pin')
+    if user_pin == self.pin:
+      # allow to withdraw
+      amount = int(input('enter the amount'))
+      if amount <= self.__balance:
+        self.__balance = self.__balance - amount
+        print('withdrawl successful.balance is',self.__balance)
+      else:
+        print('abe garib')
+    else:
+      print('sale chor')
+
+
+# need for static vars
+class Atm:
+  __counter = 1
+  # constructor(special function)->superpower -> 
+  def __init__(self):
+    print(id(self))
+    self.pin = ''
+    self.__balance = 0
+    self.cid = Atm.__counter
+    Atm.__counter = Atm.__counter + 1
+    #self.menu()
+
+  # utility functions
+  @staticmethod
+  def get_counter():
+    return Atm.__counter
+  def get_balance(self):
+    return self.__balance
+
+  def set_balance(self,new_value):
+    if type(new_value) == int:
+      self.__balance = new_value
+    else:
+      print('beta bahot maarenge')
+
+  def __menu(self):
+    user_input = input("""
+    Hi how can I help you?
+    1. Press 1 to create pin
+    2. Press 2 to change pin
+    3. Press 3 to check balance
+    4. Press 4 to withdraw
+    5. Anything else to exit
+    """)
+
+    if user_input == '1':
+      self.create_pin()
+    elif user_input == '2':
+      self.change_pin()
+    elif user_input == '3':
+      self.check_balance()
+    elif user_input == '4':
+      self.withdraw()
+    else:
+      exit()
+
+  def create_pin(self):
+    user_pin = input('enter your pin')
+    self.pin = user_pin
+
+    user_balance = int(input('enter balance'))
+    self.__balance = user_balance
+
+    print('pin created successfully')
+
+  def change_pin(self):
+    old_pin = input('enter old pin')
+
+    if old_pin == self.pin:
+      # let him change the pin
+      new_pin = input('enter new pin')
+      self.pin = new_pin
+      print('pin change successful')
+    else:
+      print('nai karne de sakta re baba')
+
+  def check_balance(self):
+    user_pin = input('enter your pin')
+    if user_pin == self.pin:
+      print('your balance is ',self.__balance)
+    else:
+      print('chal nikal yahan se')
+
+  def withdraw(self):
+    user_pin = input('enter the pin')
+    if user_pin == self.pin:
+      # allow to withdraw
+      amount = int(input('enter the amount'))
+      if amount <= self.__balance:
+        self.__balance = self.__balance - amount
+        print('withdrawl successful.balance is',self.__balance)
+      else:
+        print('abe garib')
+    else:
+      print('sale chor')
+
+
+
